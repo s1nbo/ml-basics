8f1133b15822275e0f931e06b2ea51472d070732
s1nbo <77055060+s1nbo@users.noreply.github.com>
2025-12-16 00:47:03 +0100

    added pretrained model

--- rl/basics/cartpole.py
+++ rl/basics/cartpole.py
@@ -1,17 +1,25 @@
 import gymnasium as gym
 from stable_baselines3 import PPO
+import os 
+
+# Training
+model_path = "cartpole_model.zip"
+
+if os.path.exists(model_path):
+    model = PPO.load(model_path)
+    print("Model loaded from", model_path)
+else:
+    train_env = gym.make("CartPole-v1")
+    model = PPO("MlpPolicy", train_env, verbose=0)
+    model.learn(total_timesteps=50000)
+    train_env.close()
+    model.save(model_path)
+    print("Training complete!")
 
 
-train_env = gym.make("CartPole-v1")
 
-model = PPO("MlpPolicy", train_env, verbose=0)
-model.learn(total_timesteps=100000)
-train_env.close()
-print("Training complete!")
-
+# Evaluation
 env = gym.make("CartPole-v1", render_mode="human")
-
-
 
 for i in range(3):
     obs, info = env.reset()
